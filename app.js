d9e633349db7873502cc12c41e3f01f4e67381f4
Joshua Daisey <Joshu2020281@live.ytech.edu>
2019-12-18 09:15:37 -0500

    Added an api to get information from our github repo

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require('express'); // Express framework
 const app = express();
+const request = require('request')
+const options = {
+  url: 'http://api.github.com/repos/josh-daisey/Spartan-DB',
+  method: 'GET',
+  headers: { 'User-Agent': 'Mozilla/5.0' }
+}
 
 let session = require('express-session');
 // Set up session with express
@@ -22,6 +28,16 @@ app.use(bodyParser.json());
 
 //Set up web folders
 app.use(express.static("public"));
+
+app.get("/github", function (req, res) {
+	request(options, function(request_err, request_res, request_body) {
+        if (request_err || request_res.statusCode != 200) {
+            res.send("Oops! There was a problem with the request module: <br>" + request_err);
+        } else {
+            res.status(200).send(request_body);
+        }
+    })
+});
 
 app.listen(3001, function() {
 	console.log('Listening on port ' + 3001 + '.');
