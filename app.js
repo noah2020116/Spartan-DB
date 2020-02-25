d9dfc1e28fc51d3130388324a06f3d60911bfc97
josh-daisey <joshu2020281@live.ytech.edu>
2020-02-25 11:05:09 -0500

    Base DB almost finished

--- app.js
+++ app.js
@@ -1,54 +1,90 @@
-const sqlite3 = require('sqlite3');
-var express = require('express');
-var session = require('express-session');
-var bodyParser = require('body-parser');
-var path = require('path');
+let sqlite3 = require('sqlite3');
+let express = require('express');
+let session = require('express-session');
+let bodyParser = require('body-parser');
+let path = require('path');
+let url = require('url');
+let pug = require('pug');
 
-const db = new sqlite3.Database('db/database.db');
+let db = new sqlite3.Database('db/database.db');
 
-var app = express();
+let app = express();
+ 
+// sets pug as the default engine for website
+app.set('view engine', 'pug')
+
+// sets the folder for pugs templates
+app.set("views", path.join(__dirname, "views"));
+
 
 app.use(session({
 	secret: 'secret',
 	resave: true,
 	saveUninitialized: true
 }));
+
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
 
-app.get('/', function(request, response) {
-    response.sendFile(path.join(__dirname + '/html/login.html'));
+function generateKey() {
+	let result           = '';
+	let characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	let charactersLength = characters.length;
+	for (let i = 0; i < 32; i++ ) {
+	   result += characters.charAt(Math.floor(Math.random() * charactersLength));
+	}
+	return result;
+ }
+
+ function setUserKey(username){
+
+	db.run('UPDATE accounts SET key = ? WHERE username = ?', [generateKey(), username], function(err) {
+	if (err) {
+		return console.error(err.message);
+		}
+	});
+}
+
+app.get('/', function(req, res) {
+	
+	res.render("login", {ip: req.query.ip});
+
 });
 
-app.post('/auth', function(request, response) {
-	let username = request.body.username;
-	let password = request.body.password;
+app.post('/auth', function(req, res) {
+	let username = req.body.username;
+	let password = req.body.password;
+	let ip = req.body.ip;
+
 	if (username && password) {
 		db.get('SELECT * FROM accounts WHERE username = \'' + username + '\' AND password = \'' + password + '\';', function(err, results) {
 			if (err)
 				console.log(err);
 			if (results) {
-				request.session.loggedin = true;
-				request.session.username = username;
-				response.redirect('/home');
+				setUserKey(username)
+				db.get('SELECT * FROM accounts WHERE username = \'' + username + '\' ;', function(err, results){
+					res.redirect(url.format({
+					pathname: ip,
+					query: {
+						key: results['key']
+					}
+					
+				}));
+				res.end();
+				});
+				
 			} else {
-				response.send('Incorrect Username and/or Password!');
+				res.send('Incorrect Username and/or Password!');
+				res.end();
 			}
-			response.end();
+				
 		});
 	} else {
-		response.send('Please enter Username and Password!');
-		response.end();
+		res.send('Please enter Username and Password!');
+		res.end();
 	}
-});
-
-app.get('/home', function(request, response) {
-	if (request.session.loggedin) {
-		response.send('Welcome back, ' + request.session.username + '!');
-	} else {
-		response.send('Please login to view this page!');
-	}
-	response.end();
 });
 
 app.listen(3000);
+
+console.log("Server starting on port 3000")
